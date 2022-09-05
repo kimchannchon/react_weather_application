d8a0265dd730d1713582f400051f437f52222ea8
root <kimchannchon101@gmail.com>
2022-09-05 11:11:38 +0700

    react-hook API-weather axios-library fetching-API_data error_handling access-JSON UI-components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [allData, setAllData] = useState({
+    city:'',
+    country:'',
+    temperature:''
+  })
+
+  useEffect(() => {
+    // actions after rendering
+    // fetch the database info - API to request weather from WEATHER DATABASE
+    fetchData()
+  },[])
+
+  const fetchData = async (city) => {
+    // try catch - error handling
+    try{
+      const APIKEY = 'fc508fd00391873c1774a7c00017bb7e'
+      // axios - library to make request to the backend with promise
+      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${'tokyo'}&appid=${APIKEY}&units=metric`)
+      await setAllData({
+        city: result.data.name,
+        country: result.data.sys.country,
+        temperature: result.data.main.temp
+      })
+    } catch (e) {
+      console.log('API - Down')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Under <main> -form -display results
+    <main>
+      <div className="App">
+        {console.log('>>TEST Success>>', allData.country, allData.city, allData.temperature)}
+        <section>
+          <h1>{allData.city}</h1>
+          <h2>{allData.country}</h2>
+          <h3>TEMPERATURE:</h3>
+          <p>{allData.temperature}°F</p>
+        </section>
+      </div>
+    </main>
   );
 }
 
